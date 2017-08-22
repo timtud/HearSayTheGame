6e571f42a5d2399050d2a9dbce0f90cb68705468
Adnan Ahmed <adnan.ahmed01@gmail.com>
2017-08-22 11:04:04 +0100

    Routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :users, only: [:show]
+
+  resources :followers, only: [:create, :destroy]
+
+  resources :rounds, only: [:show, :update]
+
+  resources :round_questions, only: [:create, :update] do
+    resources :rounds, only: [:create]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
