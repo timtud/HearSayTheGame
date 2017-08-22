3e85bc754594905c5c0987e26af62f3153ec2c9f
tim degroot <tdegroot4390@gmail.com>
2017-08-22 12:27:10 +0100

    setup page

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,8 @@ Rails.application.routes.draw do
     resources :rounds, only: [:create]
   end
 
+  Rails.application.routes.draw do
+    get 'pages/leaderboard'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
