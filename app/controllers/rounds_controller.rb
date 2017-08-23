fa9fa6750417fefde90c5ab8e2fe903e14c84a4d
tim degroot <tdegroot4390@gmail.com>
2017-08-23 12:37:40 +0100

    can go trough all question without answering

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -4,27 +4,36 @@ class RoundsController < ApplicationController
   def show
     @round = Round.find(params[:id])
     # maybe display final results
+    if @round.round_questions.where(answered: false).first
+      answer = @round.round_questions.where(answered: false).first.question.answer
+      @answers = Answer.where.not(name: answer.name).order("RANDOM()")[0..2]
+      @answers << answer
+      @answers.shuffle!
+    else
+      redirect_to root_path
+    end
   end
 
   def update
+    @round = Round.find(params[:id])
+    answer = @round.round_questions.where(answered: false).first
+    answer.update(answered: true)
+    if answer.save
+      redirect_to @round
+    else
+      redirect_to root_path
+    end
   end
 
   def create
     #Creates 10 random questions
-    questions = []
-    10.times do
-      new_question = Question.order("RANDOM()").first
-      until questions.exclude? new_question
-        new_question = Question.order("RANDOM()").first
-      end
-      questions << new_question
-    end
-    @round = Round.build
+    questions = Question.order("RANDOM()")[0..9]
+    @round = Round.create(user_id: current_user.id, score: 0, correct_count: 0)
     questions.each do |q|
-      @round.round_questions.create(q)
+      @round.round_questions.create(question: q, answered: false)
     end
     if @round.save
-      redirect_to @round.round_questions.first
+      redirect_to @round
     else
       redirect_to root_path
     end
