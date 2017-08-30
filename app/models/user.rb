12a5865229440b9a150f2652c50899e164d1e26b
Charlie Merrill <fcharliemerrill@gmail.com>
2017-08-30 12:22:06 +0100

    country now displays full country name

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,7 +51,7 @@ class User < ApplicationRecord
   end
 
   def country_name
-    country = ISO3166::Country[country_code]
+    country = ISO3166::Country[location]
     country.translations[I18n.locale.to_s] || country.name
   end
 
